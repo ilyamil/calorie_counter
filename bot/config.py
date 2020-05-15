36b5443e59e0f2f49f7dff3498ecc163f59d2382
ilyamil <44316860+ilyamil@users.noreply.github.com>
2020-05-15 16:35:36 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
-TOKEN = "1116346383:AAFrG35Or8m-YZr9bAnx_7RrpHdoq9ZjX8w"
+TOKEN = <YOUR TOKEN>
 BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 FILE_URL = "https://api.telegram.org/file/bot"
 MODEL = "bot/model/yolo_open_images.h5"
