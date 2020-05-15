f6fb4b403a27b2a6f70f75fce2fa0adfb90ffbda
ilyamil <44316860+ilyamil@users.noreply.github.com>
2020-05-15 16:33:25 +0300

    first version

---
+++ bot/config.py
@@ -0,0 +1,6 @@
+TOKEN = "1116346383:AAFrG35Or8m-YZr9bAnx_7RrpHdoq9ZjX8w"
+BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+FILE_URL = "https://api.telegram.org/file/bot"
+MODEL = "bot/model/yolo_open_images.h5"
+FOOD_CLASSES = "bot/model/food_classes.txt"
+ALL_CLASSES = "bot/model/classes.txt"
